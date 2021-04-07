d7fc5ce768a49be61abeefb41ba03d4dbfa221dd
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-04-07 12:53:55 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn django_tasklist.wsgi --log-file -
+
---
+++ ticaretDjangoAPI_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TicaretdjangoapiAppConfig(AppConfig):
+    name = 'ticaretDjangoAPI_app'
